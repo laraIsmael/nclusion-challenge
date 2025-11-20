0366aa6d42d6b877ec9250ac4af9b1f216d85cb4
Lara Ismael <lara8ismael@gmail.com>
2025-11-20 11:33:18 -0800

    final updates and optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import ComplexGrid from "./components/ComplexGrid";
 
 function App() {
-  const size = 600;
-  const maxIter = 500;
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className=" py-10 text-3xl font-bold text-teal-700">
         Nclusion-challenge
       </h1>
-      <ComplexGrid width={size} height={size} maxIter={maxIter} />
+      <ComplexGrid />
       <div className="mt-4 p-3 bg-white border border-gray-200 rounded-lg shadow-md text-sm text-teal-800">
         <p className="font-semibold mb-1">Visualization Specifications:</p>
         <ul className="list-disc list-inside space-y-1">
           <li>Grid Range: (-2, -2) to (2, 2) in the complex plane.</li>
           <li>
-            Resolution: {size}x{size} (360,000 squares).
+            Resolution: {600}x{600} (360,000 squares).
           </li>
           <li>
             Coloring: <span className="text-teal-900 font-bold">Light</span> for
