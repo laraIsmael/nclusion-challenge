f2311a1e6a7b72a4aa4716cfb8d2849d39dea26c
Lara Ismael <lara8ismael@gmail.com>
2025-12-15 11:32:35 -0800

    reset for interview

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // import ComplexGrid from "./components/ComplexGrid";
-import ComplexGridUpgrade from "./components/ComplexGridUpgrade.tsx";
+import ComplexGrid from "./components/ComplexGrid.tsx";
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className=" py-10 text-3xl font-bold text-teal-700">
         Nclusion-challenge
       </h1>
-      <ComplexGridUpgrade />
+      <ComplexGrid />
       <div className="mt-4 p-3 bg-white border border-gray-200 rounded-lg shadow-md text-sm text-teal-800">
         <p className="font-semibold mb-1">Visualization Specifications:</p>
         <ul className="list-disc list-inside space-y-1">
--- src/components/ComplexGrid.tsx
+++ src/components/ComplexGrid.tsx
@@ -1,95 +1,10 @@
 import { useEffect, useRef } from "react";
-// import type { Complex } from "../utils/complex";
-// import { iterate } from "../utils/iterate";
-
-// // type Props = {
-// //   width?: number; // grath x axis size
-// //   height?: number; // grath y axis size
-// //   maxIter?: number; // number of iteration before point escaped
-// // };
-
-// // const OldComplexGrid: React.FC<Props> = ({
-// //   width = 500,
-// //   height = 500,
-// //   maxIter = 200,
-// // }) => {
-// //   // reference to the canvas DOM element
-// //   const canvasRef = useRef<HTMLCanvasElement>(null);
-
-// //   // after the component is rendered:
-// //   useEffect(() => {
-// //     // grab the canvas element
-// //     // sanaty check canvas exist
-// //     const canvas = canvasRef.current;
-// //     if (!canvas) return;
-
-// //     // creating the canvas context in 2D
-// //     // sanaty check context exisit
-// //     const ctx = canvas.getContext("2d");
-// //     if (!ctx) return;
-
-// //     // creating the grid x(-2,2) y(-2,2)
-// //     const xMin = -2; // left most
-// //     const xMax = 2; // right most
-// //     const yMin = -2; // bottom most
-// //     const yMax = 2; // top most
-
-// //     // empty grath of size width x height - to be filled with color later
-// //     const imageData = ctx.createImageData(width, height);
-
-// //     // map each pixel to complex plane
-// //     for (let px = 0; px < width; px++) {
-// //       for (let py = 0; py < height; py++) {
-// //         const x = xMin + (px / width) * (xMax - xMin);
-// //         const y = yMax - (py / height) * (yMax - yMin);
-
-// //         // making every pixel inot a complex number
-// //         const c: Complex = { re: x, im: y };
-
-// //         // check: how many loops we go through to identify if the point escape or stayed bounded
-// //         const iter = iterate(c, maxIter);
-
-// //         // now converting iteration count into color - gray scale
-// //         // escape early - dark color escaped
-// //         // takes longer to escape - lighter color eventually escaped
-// //         // never escapes - full white stayed bounded
-// //         const color = Math.floor((iter / maxIter) * 255);
-
-// //         // find the specific pixel location - the imageData is store in a very long 1dimentional array.
-// //         // row * width: finds the start of the desired row
-// //         // + column: gets to the specific column in said row
-// //         // * 4: specific 4 bytes that makes the pixel
-// //         const idx = (py * width + px) * 4;
-
-// //         // adding color to each byte of the pixel by location
-// //         imageData.data[idx] = color; //R
-// //         imageData.data[idx + 1] = color; //G
-// //         imageData.data[idx + 2] = color; //B
-// //         imageData.data[idx + 3] = 255; //A
-// //       }
-// //     }
-
-// //     // render the canvas with color collected on the iteration above
-// //     ctx.putImageData(imageData, 0, 0);
-// //   }, [width, height, maxIter]);
-
-// //   return (
-// //     <canvas
-// //       ref={canvasRef}
-// //       width={width}
-// //       height={height}
-// //       className="border border-teal-600"
-// //     />
-// //   );
-// // };
-
-// optimized code:
 
 const WIDTH = 600;
 const HEIGHT = 600;
 const MAX_ITER = 500;
 
-function OptimizedComplexGrid() {
+function ComplexGrid() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   // Iterate z(n+1) = z(n)^2 + c
@@ -157,4 +72,4 @@ function OptimizedComplexGrid() {
   );
 }
 
-export default OptimizedComplexGrid;
+export default ComplexGrid;
